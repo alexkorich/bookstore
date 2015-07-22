1d4b90bf82f6674010e5ffcb58dbaa032c201707
Alexey Korichkovskiy <johniezigger@gmail.com>
2015-07-22 15:31:50 +0300

    some devise

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,10 @@
 class Customer < ActiveRecord::Base
-  validates :email, :password, :firstname, :lastname presence: true
+  validates :email, :password, :firstname, :lastname, presence:true
   validates :email, uniqueness:true
   has_many :orders
   has_many :ratings
   
+  def self.create_order
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  resources :books
+  resources :customers
+  resources :authors
+  resources :categories
+  resources :orders
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # 
