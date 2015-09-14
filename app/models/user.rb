c67ca660b1f0974b979f6a0870538432cbecf862
alexkorich <johniezigger@gmail.com>
2015-09-15 00:15:16 +0300

    facebook log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,14 +21,15 @@ class User < ActiveRecord::Base
 def self.from_omniauth(auth)
   # split name
   puts "TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT"
-  puts auth.info
+  puts auth.info[:email]
+  puts "KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
   puts auth
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.email
+  where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+    user.email = auth.info[:email]
     user.password = Devise.friendly_token[8,20]
-    user.firstname = auth.firstname
-    user.lastname = auth.lastname   # assuming the user model has a name
-    user.avatar = auth.info.image # assuming the user model has an image
+    user.firstname = auth.info[:first_name]
+    user.lastname = auth.info[:last_name]   # assuming the user model has a name
+    user.avatar = auth.info[:image] # assuming the user model has an image
   user
   end
 
