c688f70d89c51ad90b0e596e1f381da8a687cc5d
alexkorich <johniezigger@gmail.com>
2015-10-27 03:41:08 +0200

    fix

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV.fetch("AWS_ACCESS_KEY_ID"),
-      :aws_secret_access_key  => ENV.fetch("AWS_SECRET_ACCESS_KEY"),
+      :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],
+      :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],
       :region                 => 'us-standart' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = "bookstore-korich-assets"
