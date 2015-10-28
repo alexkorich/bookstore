d72f419ad85d985e16e960e039e0cc0b5e82acac
alexkorich <johniezigger@gmail.com>
2015-10-28 10:59:40 +0200

    travis

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -29,7 +29,7 @@ class Ability
     # For example, here the user can only update published articles.
     #
     #   can :update, Article, :published => true
-    #
+    #xxx
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
   end
