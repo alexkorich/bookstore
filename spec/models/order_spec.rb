c585ef1cfb8a2b6415b11b14e18fba272404b2b8
alexkorich <johniezigger@gmail.com>
2015-10-30 00:47:51 +0200

    remov precomp assets

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -13,11 +13,31 @@ RSpec.describe Order, type: :model do
     it {should have_many(:order_items)}
 
     context 'active_or_adress' do
+      it {should validate_presence_of(:billing_adress)}
+      it {should validate_presence_of(:shipping_adress)}
+
+    end
+
+    context 'active_or_delivery' do
+      
+    end
+   context 'active_or_payment' do
+      
+    end
+    context 'active' do
       
     end
 
-       context 'active_or_adress' do
-      
-    end
+
+
+
+
+
+
+
+
+
+
+
 
 end
