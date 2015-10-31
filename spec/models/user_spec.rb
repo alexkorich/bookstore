b0464c42a7cb0956957fc0e2b684e1ec2ac25e1b
alexkorich <johniezigger@gmail.com>
2015-10-31 04:07:24 +0200

    tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,27 +13,36 @@ RSpec.describe User, type: :model do
   context "instance methods" do
 
 
-# wrap into currnt order
-    it "creates new order if no present" do
-      expect(user.current_order).to be_a Order
-      expect(user.current_order.state).to eq("in_progress")
-    end
-    it "returns current_order" do
-      expect(order.user.current_order).to eq(order)
-      expect(order.user.current_order.state).to eq("in_progress")
-    end
-
-    it "adds a new order to existing" do
-
+   describe '#current_order' do
+     it "creates new order if no present" do
+       expect(user.current_order).to be_a Order
+       expect(user.current_order.state).to eq("in_progress")
+     end
+     it "returns current_order" do
+       expect(order.user.current_order).to eq(order)
+       expect(order.user.current_order.state).to eq("in_progress")
+     end
+   end
+   describe '#create_order' do
+     it "adds a new order to existing" do
       orders_count=user.orders.count
       user.create_order
       expect(user.orders.count).to eq(orders_count+1)
+     end
+  end
+   describe '#send_admin_mail' do
+    it "adds mail counter" do
+      expect {user.send_admin_mail}.to change {ActionMailer::Base.deliveries.count }.by(2)
+
     end
 
-
-  end
-  context "class methods" do
-    
+end
+context 'class methods ' do
+   describe '.from_omniauth' do
+end
+ describe '.find_for_oauth' do
+end
+end
   end
 
 end
